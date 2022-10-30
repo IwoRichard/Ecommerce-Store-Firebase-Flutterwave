adf30adbb657dfa4366979be5bc8b662bd39d671
Iwo Ricahrd <richardiwo1@gmail.com>
2022-10-30 04:13:36 +0100

    redo

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 // ignore_for_file: prefer_const_constructors
 
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:shop/screens/home_page.dart';
-import 'package:shop/screens/signUp_page.dart';
-import '../services/Firebase_Auth.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -18,15 +16,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: StreamBuilder(
-        stream: AuthService().firebaseAuth.authStateChanges(),
-        builder: (context, AsyncSnapshot snapshot) {
-          if (snapshot.hasData) {
-            return HomePage();
-          }
-          return RegisterScreen(user: snapshot.data);
-        }
-      ),
     );
   }
 }
--- lib/models/user_info.dart
+++ lib/models/user_info.dart
@@ -1,12 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class UserInfo {
-  String userId;
   String name;
   String address;
   Timestamp date;
   UserInfo({
-    required this.userId,
     required this.name,
     required this.address,
     required this.date,
@@ -14,7 +12,6 @@ class UserInfo {
 
   factory UserInfo.fromJson(DocumentSnapshot snapshot){
     return UserInfo(
-      userId: snapshot['userId'], 
       name: snapshot['name'], 
       address: snapshot['address'], 
       date: snapshot['date']
