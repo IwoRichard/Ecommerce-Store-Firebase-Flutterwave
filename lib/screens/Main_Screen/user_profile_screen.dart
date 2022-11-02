6659ff4bee37bfacef12e294c85ff92370de0e17
Iwo Ricahrd <richardiwo1@gmail.com>
2022-11-02 13:18:27 +0100

    delete stuff

--- lib/screens/Main_Screen/user_profile_screen.dart
+++ lib/screens/Main_Screen/user_profile_screen.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: prefer_const_constructors, avoid_print
 
-import 'package:cloud_firestore/cloud_firestore.dart';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -12,37 +12,9 @@ class UserProfileScreen extends StatefulWidget {
 }
 
 class _UserProfileScreenState extends State<UserProfileScreen> {
-
-  String? email;
-  String? name;
-  String? address;
   final User? user = FirebaseAuth.instance.currentUser;
-
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('userInfo').where('userId', isEqualTo: user!.uid).snapshots(),
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  address!
-                ),
-                Text(
-                  name!
-                ),
-                Text(
-                  email!
-                )
-              ],
-            )
-          );
-        },
-      ),
-    );
+    return Scaffold();
   }
 }
