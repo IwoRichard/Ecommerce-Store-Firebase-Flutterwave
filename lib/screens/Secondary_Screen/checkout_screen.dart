b88616b7ccb33cd228c52c2a9adb4a41649e32a1
Iwo Ricahrd <richardiwo1@gmail.com>
2022-12-10 18:54:00 +0100

    fontsize

--- lib/screens/Secondary_Screen/checkout_screen.dart
+++ lib/screens/Secondary_Screen/checkout_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously, non_constant_identifier_names
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -102,7 +104,10 @@ class _CheckOutScreenState extends State<CheckOutScreen> {
   ListTile amountListTile(String amount, String title) {
     return ListTile(
       leading:  Text(title),
-      trailing: Text('\$$amount'),
+      trailing: Text(
+        '\$$amount',
+        style: const TextStyle(fontSize: 17,fontWeight: FontWeight.w600),
+      ),
     );
   }
 
